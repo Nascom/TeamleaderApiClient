078de746db3df88377cab2a743928d333e0ec499
Maarten De Block <info@maartendeblock.be>
2019-03-12 15:56:56 +0100

    Changed comment on Class from Email to BusinessType

--- src/Entity/BusinessType.php
+++ src/Entity/BusinessType.php
@@ -4,7 +4,7 @@ namespace Nascom\TeamleaderApiClient\Entity;
 use Nascom\TeamleaderApiClient\Entity\Entity;
 
 /**
- * Class Email
+ * Class BusinessType
  *
  * @package Nascom\TeamleaderApiClient\Entity
  */
